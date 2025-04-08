beaa93fee9bfc857199107488b35edcceadda9a9
TheDevCaio <caioinnocencio@gmail.com>
2025-04-08 16:15:25 -0300

    Criando o serviço.

---
+++ Aula/src/main/java/com/ufjf/Aula/entities/Tarefa.java
@@ -0,0 +1,11 @@
+package com.ufjf.Aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Tarefa {
+
+private int id;
+private String descricao;
+private boolean
+}
--- Aula/src/main/java/com/ufjf/Aula/service/ServiceTarefa.java
+++ Aula/src/main/java/com/ufjf/Aula/service/ServiceTarefa.java
@@ -1,13 +1,51 @@
 package com.ufjf.Aula.service;
 
-
-@
+@Service
 public class ServiceTarefa{
 
     List<Tarefa> getTarefas(){
         return tarefa;
+    }
+
+    public void addTarefa(String descrica) {
+        Tarefa tarefa = new Tarefa(descricao);
+        Tarefa.add(tarefa);
 
     }
 
+    public List<Tarefa> listar() {
+       return tarefas;
+    }
 
+    public Tarefa buscarPorId(int id) {
+        for (Tarefa t >: tarefas) {
+            if(t.getid() == id){
+                return t;
+            }
+            return null
+        }
+    }
+
+    public void removerPorId(*ijnt id) {
+        FOR(tarefa t: tarefas) {
+            if(t.getId() == id){
+                tarefas.remove(t);
+                break;
+            }
+        }
+    }
+
+    public void atualizarPorID(string name){
+    for(Tarefa t : tarefas) {
+
+
+    }
+}
+
+    public void atualizarStatusPorId(int id){
+        for( Tarefa t : tarefas ) {
+            if(t.getId()== id) {
+
+            }
+    }
 }
